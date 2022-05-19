13671e7e6ef0bcd419752a1cc215b92735daa1d6
N1kola0s <97747012+N1kola0s@users.noreply.github.com>
2022-05-19 15:40:47 +0200

    add length of text on screen

--- index.php
+++ index.php
@@ -1,8 +1,12 @@
 <?php
+    /* Creare una variabile con un paragrafo di testo a vostra scelta. */
+    $text="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aspernatur dignissimos harum veniam at ex, recusandae ipsum nisi omnis dolorem et!";
+    /* stampo a schermo contenuto variabile e la sua lunghezza */
+    var_dump($text);
+    var_dump(strlen($text)); 
 
-    $text="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aspernatur dignissimos harum veniam at ex, recusandae ipsum nisi omnis dolorem et!"
-
-    
+    /* inserisco lunghezza testo in una variabile */
+    $lenght = strlen($text);
 
 ?>
 
@@ -15,6 +19,10 @@
     <title>Document</title>
 </head>
 <body>
+    <!-- Stampo a schermo il paragrafo -->
     <p>Sono un testo di prova <?php echo $text; ?></p>
+    <!-- Stampo a schermo la sua lunghezza -->
+    <p>La lunghezza del paragrafo è di <?php echo $lenght; ?> caratteri </p>
+
 </body>
 </html>
