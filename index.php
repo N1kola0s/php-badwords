78f45b3bd37fea49b261ce15341d098afc85e697
N1kola0s <97747012+N1kola0s@users.noreply.github.com>
2022-05-19 15:32:39 +0200

    Initial commit
    add php variable and stamp it on screen

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+    $text="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aspernatur dignissimos harum veniam at ex, recusandae ipsum nisi omnis dolorem et!"
+
+    
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>Sono un testo di prova <?php echo $text; ?></p>
+</body>
+</html>
